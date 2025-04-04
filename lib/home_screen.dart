2f25da7a25396fad9860ae1dbcdff32837da6a0e
parth-benzatine <parth@benzatine.com>
2025-04-04 17:32:19 +0530

    app2 plugin added!!

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,21 @@
+import 'package:app2/home_screen.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(title: Text("Application1")),
+      appBar: AppBar(title: Text('Main App')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open App2'),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
+          },
+        ),
+      ),
     );
   }
 }
