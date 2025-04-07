2f93210a5271d27f2240b636c2382c7eae0e8efb
parth-benzatine <parth@benzatine.com>
2025-04-07 10:33:46 +0530

    app1 and app2 packages added !!

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,31 +1,37 @@
+import 'package:app1/app1.dart';
+import 'package:app2/app2.dart';
 import 'package:flutter/material.dart';
 
 class MainHomeScreen extends StatelessWidget {
+  const MainHomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Main App')),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              child: Text('Open App1'),
-              onPressed: () {
-                // Navigator.of(context).push(
-                //   MaterialPageRoute(builder: (context) => HomeScreen()),
-                // );
-              },
-            ),
-            ElevatedButton(
-              child: Text('Open App2'),
-              onPressed: () {
-                // Navigator.of(context).push(
-                //   MaterialPageRoute(builder: (context) => HomeScreen()),
-                // );
-              },
-            ),
-          ],
-        ),
+      appBar: AppBar(title: const Text("Main App")),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const App1HomeScreen(),
+                  ));
+            },
+            child: const Text("Open App 1"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const App2HomeScreen(),
+                  ));
+            },
+            child: const Text("Open App 2"),
+          ),
+        ],
       ),
     );
   }
